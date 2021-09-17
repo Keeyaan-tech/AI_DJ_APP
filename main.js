d373c2fa500281cd7705d51de3f8cca5c7487b61
Keeyaan-tech <78153976+Keeyaan-tech@users.noreply.github.com>
2021-09-17 18:44:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 song = "";
-function preload(){
-song = loadSound("music.mp3");
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+
+
+
+function preload() {
+    song = loadSound("music.mp3");
 }
 
 function setup() {
@@ -9,12 +17,35 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
-    image(video,0,0,600,500);
+    image(video, 0, 0, 600, 500);
 }
 
-function play(){
-song.play();
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function modelLoaded() {
+    console.log('Posenet is initialised');
+}
+
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+leftWristX = results[0].pose.leftWristX.x;
+leftWristY = results[0].pose.leftWristY.y;
+console.log("leftWristX = " + leftWristX + ". leftWristY = " + leftWristY);
+
+rightWristX = results[0].pose.rightWristX.x;
+rightWristY = results[0].pose.rightWristY.y;
+console.log("rightWristX = " + rightWristX + ". rightWristY = " + rightWristY);
+
+}
 }
